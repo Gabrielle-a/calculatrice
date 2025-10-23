2672269e7971b205701fdfbf7a2e52abf958ac78
Gabrielle <tayegabrielle616@gmail.com>
2025-10-23 21:01:52 +0200

    feat(utils): add add(*args) and subtract(*args)

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+def add(*args):
+    """Return the sum of all numbers in args."""
+    total = 0
+    for x in args:
+        total += x
+    return total
+
+
+def subtract(*args):
+    """
+    Subtract all subsequent numbers from the first.
+    subtract(10, 1, 2) -> 7
+    """
+    if not args:
+        raise ValueError("subtract() requires at least one number")
+    result = args[0]
+    for x in args[1:]:
+        result -= x
+    return result
